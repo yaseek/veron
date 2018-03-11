0f25251661e9cb79524fffe1ff419f8db71ec674
Andrew Babkin <ab@izh.com>
2018-03-11 17:50:45 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 //import { additionalFields, AdditionalForm } from './components'
 
-const LOGIN_URL = 'http://veron.myjino.ru/css/1/index1.php'
-const REGISTRATION_URL = 'http://veron.myjino.ru/css/1/reg.php'
+const LOGIN_URL = '/vhod.php'
+const REGISTRATION_URL = '/reg.php'
 
-// const marked = new WeakMap()
+const marked = new WeakMap()
 
 // let count = 0
 
 const noClick = (e) => e.stopPropagation()
 
-const buttonClick = (e) => {
+const buttonClick = (url) => (e) => {
     e.stopPropagation()
     e.preventDefault()
 
-    window.location.href = REGISTRATION_URL
+    window.location.href = url
+}
+
+const fbIds = {
+    "Алиса Погарская": "1234",
+    "Michael A. Soares": "1234",
+    "Ailien Phan": "1234",
+    "Adriana Ramon": "1234",
+    "Kyle Rankin": "1234",
+    "Robert Rodden": "1234",
+    "Pavlo Grozian": "1234",
+    "Josh Wright": "1234",
+    "Jamie Radice": "1234",
+}
+
+const renderContact = (node, contactName) => {
+    const fbId = fbIds[contactName]
+
+    if (!marked.get(node) && fbId) {
+        node.innerHTML = `<span>${contactName}</span>
+            <a class="icon_facebook" href="https://www.facebook.com/${fbId}">
+                <img src="/img/facebook.svg" />
+            </a>`;
+        marked.set(node, true)
+    }
 }
 
 const nodeInserted = (node) => {
@@ -58,10 +82,24 @@ const nodeInserted = (node) => {
         const item = buttonsCollection.item(i)
 
         if (item.innerText.trim().toUpperCase() === 'РЕГИСТРАЦИЯ') {
-            item.addEventListener('click', buttonClick)
+            item.addEventListener('click', buttonClick(REGISTRATION_URL))
+        }
+
+        if (item.innerText.trim().toUpperCase() === 'ВХОД') {
+            item.addEventListener('click', buttonClick(LOGIN_URL))
         }
     }
     
+    /*---------------- CONTACTS ---------------------------------*/
+
+    const contacts = relatedNode.querySelectorAll('.AboutUs_staff__heading_1TiRm')
+
+    if (contacts && contacts.length) {
+        for (const contact of contacts.values()) {
+            renderContact(contact, contact.innerHTML)
+        }
+    }
 }
 
 window.addEventListener('DOMNodeInserted', nodeInserted)
+
